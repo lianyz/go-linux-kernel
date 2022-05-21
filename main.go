33f88e70b597f6884f3ad6c28cb33a87a92b4b83
lianyz <lianyz@email.cn>
2022-05-21 13:33:34 +0800

    child strategy

--- main.go
+++ main.go
@@ -16,20 +16,30 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("help")
 	}
 }
 
 func run() {
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS,
 	}
-	must(syscall.Sethostname([]byte("container1")))
+	must(cmd.Run())
+}
+
+func child() {
+	must(syscall.Sethostname([]byte("mycontainer")))
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	must(cmd.Run())
 }
 
